ccaf009721424e9e3fb3c477b3ccf9681eb59a08
Eliza Souza <elizasouza@uniplaclages.edu.br>
2020-09-23 16:28:12 -0300

    Atualizando arquivos html

--- mod_erro/erro.py
+++ mod_erro/erro.py
@@ -4,10 +4,10 @@ from flask import Blueprint, render_template, request
 
 bp_erro = Blueprint('erro', __name__, url_prefix='/erro', template_folder='templates')
 
-@bp_erro.route(404)
+@bp_erro.route("/404")
 def erro404(error):
-    return render_template("form404.html")
+    return render_template("form404.html"), 404
 
-@bp_cliente.route(500)
+@bp_erro.route("/500")
 def erro500(error):
-    return render_template("form500.html")
+    return render_template("form500.html"), 500
